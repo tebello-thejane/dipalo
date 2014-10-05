5436634ad2fbd985f17dfd8ac280cd1823ccf75d
Tebello Thejane <zyxoas@gmail.com>
2014-10-05 14:08:33 +0200

    .

--- src/dipalo.hs
+++ src/dipalo.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings, OverloadedLists, CPP #-}
-  
+
 import qualified Data.Vector as Vec
 import Data.Vector((!))
 import Data.Text hiding (map)
@@ -16,39 +16,39 @@ data NumType = NumType {full :: Text, mid :: Text, final :: Text}
 nums :: Vec.Vector NumType
 nums = Vec.fromList[
     NumType {full="lefela", mid="", final=""},
-    NumType {full="nngwe", mid="", final="eng"}, 
-    NumType {full="pedi", mid="ets", final="edi"}, 
-    NumType {full="tharo", mid="ar", final="aro"}, 
-    NumType {full="nne", mid="eny", final="ene"}, 
-    NumType {full="hlano", mid="an", final="ano"}, 
-    NumType {full="tshelela", mid="el", final="ela"}, 
-    NumType {full="supa", mid="up", final="upa"}, 
-    NumType {full="robedi", mid="ots", final="odi"}, 
-    NumType {full="robong", mid="ony", final="ong"}, 
+    NumType {full="nngwe", mid="", final="eng"},
+    NumType {full="pedi", mid="ets", final="edi"},
+    NumType {full="tharo", mid="ar", final="aro"},
+    NumType {full="nne", mid="eny", final="ene"},
+    NumType {full="hlano", mid="an", final="ano"},
+    NumType {full="tshelela", mid="el", final="ela"},
+    NumType {full="supa", mid="up", final="upa"},
+    NumType {full="robedi", mid="ots", final="odi"},
+    NumType {full="robong", mid="ony", final="ong"},
     NumType {full="leshome", mid="", final=""}
     ]
-    
+
 consts :: Map.Map Text Text
 consts = Map.fromList[
     ("10", "leshome"), ("10-pre-outside", "lesh"), ("10-pre-inside", "esh"), ("10s-pre-outside", "mash"), ("10s-pre-inside", "ash"),
     ("100", "lekgolo"), ("100-pre-outside", "lekg"), ("100-pre-inside", "ekg"), ("100s-pre-outside", "makg"), ("100s-pre-inside", "akg")
-    
+
     ]
-    
+
 #else
 
 nums :: Vec.Vector NumType
 nums = Vec.fromList[
     NumType {full="lēféla", mid="", final=""},
-    NumType {full="ńńgwē", mid="", final="ḗng"}, 
-    NumType {full="pedí", mid="ets", final="edí"}, 
-    NumType {full="thárō", mid="ár", final="árō"}, 
-    NumType {full="ńne", mid="ḗny", final="ḗne"}, 
-    NumType {full="hlánō", mid="án", final="ánō"}, 
-    NumType {full="tshḗlḗla", mid="ḗl", final="ḗla"}, 
-    NumType {full="supá", mid="up", final="upá"}, 
-    NumType {full="robedí", mid="ots", final="odí"}, 
-    NumType {full="robṓng", mid="ṓny", final="ṓng"}, 
+    NumType {full="ńńgwē", mid="", final="ḗng"},
+    NumType {full="pedí", mid="ets", final="edí"},
+    NumType {full="thárō", mid="ár", final="árō"},
+    NumType {full="ńne", mid="ḗny", final="ḗne"},
+    NumType {full="hlánō", mid="án", final="ánō"},
+    NumType {full="tshḗléla", mid="ḗl", final="ḗla"},
+    NumType {full="supá", mid="up", final="upá"},
+    NumType {full="robedí", mid="ots", final="odí"},
+    NumType {full="robṓng", mid="ṓny", final="ṓng"},
     NumType {full="lēshṓme", mid="", final=""}
     ]
 
@@ -56,11 +56,11 @@ consts :: Map.Map Text Text
 consts = Map.fromList[
     ("10", "lēshṓme"), ("10-pre-outside", "lēsh"), ("10-pre-inside", "ēsh"), ("10s-pre-outside", "mash"), ("10s-pre-inside", "ash"),
     ("100", "lēkgṓlō"), ("100-pre-outside", "lēkg"), ("100s-pre-outside", "makg"), ("100-pre-inside", "ēkg"), ("100s-pre-inside", "akg")
-    
+
     ]
-    
+
 #endif
-    
+
 group :: Int -> Text
 group x
     | x == 10  = (consts Map.! "10")
@@ -112,12 +112,12 @@ main = do
     hSetBuffering stdout NoBuffering
 
     putStr "Tlanya: "
-    
+
     x <- getLine
     let xn = (read x) :: Int
-     
+
     if xn < 0
-        then 
+        then
             putStrLn "Bye!"
         else do
             --putStrLn . unpack . Main.group $ xn
